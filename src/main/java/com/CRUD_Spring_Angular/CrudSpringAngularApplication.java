a4b044fe368f7d02c0908c3d8efdd1051a60304f
Deepak Singh <deepsinghkumar01@gmail.com>
2024-05-12 16:38:10 +0530

    Initial commit, project created with few required dependencies.

---
+++ src/main/java/com/CRUD_Spring_Angular/CrudSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.CRUD_Spring_Angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringAngularApplication.class, args);
+	}
+
+}
